79a20375599ce6f38dce9f1de2b04de0457c70ac
zonarr <poomison2013@gmail.com>
2026-03-06 19:54:22 +0700

    Update .gitignore to include *.db and enforce NOT NULL constraint on book name in database schema

--- database.py
+++ database.py
@@ -7,7 +7,11 @@ cursor = conn.cursor()
 def create_table_book():
     cursor.execute("""CREATE TABLE IF NOT EXISTS book(
                    book_id TEXT PRIMARY KEY,
-                   name TEXT,
+                   name TEXT NOT NULL,
                    finished BOOLEAN,
-                   rating REAL) """)
+                   rating REAL CHECK (rating<5 AND rating>0)
+                   ) """)
     conn.commit()
+
+if __name__ == "__main__":
+    create_table_book()
